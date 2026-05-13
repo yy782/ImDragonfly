76a239a066c7c2a2b97879a5f98c7f22f0a550a9
yy782 <736262857@qq.com>
2026-05-13 15:17:32 +0800

    compact_obj完善，支持int,str,ttl_str

--- src/sharding/DashTable/compact_obj.hpp
+++ src/sharding/DashTable/compact_obj.hpp
@@ -6,13 +6,10 @@
 #include <optional>
 #include <string>
 #include <string_view>
-
+#include <glog/logging.h>
 namespace dfly{
 
-namespace detail
-{
-class RobjWrapper;
-}
+
 
 
 
@@ -31,56 +28,9 @@ protected:
         INT_TAG = 17,
         STR_TAG = 18, // 字符串
         ROBJ_TAG = 19, // Redis 对象（list/hash/set） 
-        SDS_TTL_TAG = 24, 
+        TTL_STR_TAG = 24,  // 过期字符串
     };
 public:
-    
-    explicit CompactObj(bool is_key)
-        : is_key_{is_key}, taglen_{0} {  
-    }
-    CompactObj(std::string_view str, bool is_key) : CompactObj(is_key) {
-        SetString(str);
-    }
-
-    CompactObj(CompactObj&& cs) noexcept : CompactObj(cs.is_key_) {
-        operator=(std::move(cs));
-    };    
-
-    ~CompactObj()=default;
-
-    CompactObj& operator=(CompactObj&& o) noexcept; 
-    bool operator==(const CompactObj& o) noexcept;
-    uint64_t HashCode() const;
-    static uint64_t HashCode(std::string_view str);
-
-    void Reset();
-
-
-    void SetString(std::string_view str);
-
-    std::string ToString() const{
-        return u_.str_;
-    }
-    CompactObjType ObjType() const;
-
-    std::string_view GetSlice(std::string* scratch) const {
-
-        if (taglen_ == STR_TAG) {
-            *scratch = std::string_view(*scratch);
-            return *scratch;
-        }
-
-        if (taglen_ == SDS_TTL_TAG) {
-            return u_.str_ttl_.view();
-        }
-
-        return std::string_view{};
-    }
-
-
-protected:
-    void SetMeta(uint8_t taglen);
-
     struct TtlString {
         std::string str_;   
         uint64_t exp_ms_;  
@@ -88,19 +38,160 @@ protected:
         std::string_view view() const {
             return std::string_view(str_);
         }
-    };
+
+        bool operator==(const TtlString& o) const{
+            return str_ == o.str_ && exp_ms_ == o.exp_ms_;
+        }
+    }; 
+
+    explicit CompactObj(bool is_key)
+        : is_key_{is_key}, tag_{0} {  
+    }
+    CompactObj(std::string_view str, bool is_key) : CompactObj(is_key) {
+        SetString(str);
+    }
+    CompactObj(int64_t val) : CompactObj(false) {
+        SetInt(val);
+    } 
+    CompactObj(const TtlString& s) :CompactObj(true) {
+        SetTtlStr(s);
+    }
+    CompactObj(CompactObj&& cs) noexcept : CompactObj(cs.is_key_) {
+        operator=(std::move(cs));
+    };    
+    ~CompactObj() {
+        Reset();
+    }
+    CompactObj& operator=(CompactObj&& o) noexcept {
+        switch (o.tag_)
+        {
+        case INT_TAG:
+            SetInt(o.u_.ival_);
+            break;
+        case STR_TAG:
+            SetString(o.u_.str_);
+            break;
+        case TTL_STR_TAG:
+            SetTtlStr(o.u_.ttl_str_);
+            break;
+        default:
+            if (is_key_)
+                LOG(FATAL) << "Invalid tag: " << o.tag_; // 插入默认值会没有标签
+            break;
+        }
+
+        return *this;
+    }
+    bool operator==(const CompactObj& o) noexcept{
+        if (tag_ != o.tag_) return false;
+        switch (tag_)
+        {
+        case INT_TAG:
+            return o.u_.ival_ == u_.ival_;
+        case STR_TAG:
+            return o.u_.str_ == u_.str_;
+        case TTL_STR_TAG:
+            return o.u_.ttl_str_ == u_.ttl_str_;
+        default:
+            if (is_key_)
+                LOG(FATAL) << "Invalid tag: " << o.tag_;
+            return false;
+        }        
+    }
+
+    uint64_t HashCode() const;
+    static uint64_t HashCode(std::string_view str);
+
+    void Reset() {
+        switch (tag_)
+        {
+            using std::string;
+            case INT_TAG:
+                break;
+            case STR_TAG:
+                u_.str_.~string(); // 
+                break;
+            case TTL_STR_TAG:
+                u_.ttl_str_.~TtlString();
+                break;
+            default:
+                if (is_key_)
+                    LOG(FATAL) << "Invalid tag: " << tag_;
+                break;
+        }
+        tag_ = 0;
+    }
+
+
+    void SetString(std::string_view str) {
+        u_.str_ = str;
+        SetMeta(STR_TAG);
+    }
+    void SetInt(int64_t val) {
+        u_.ival_ = val;
+        SetMeta(INT_TAG);
+    }
+    void SetTtlStr(const TtlString& s) {
+        u_.ttl_str_ = std::move(s);
+    }
+
+
+    std::string ToString() const {
+        switch (tag_)
+        {
+        case INT_TAG:
+            return std::to_string(u_.ival_);
+        case STR_TAG:
+            return u_.str_;
+        case TTL_STR_TAG:
+            return u_.ttl_str_.str_;
+        default:
+            if (is_key_)
+                LOG(FATAL) << "Invalid tag: " << tag_;
+            return {};
+        }         
+    }
+    CompactObjType ObjType() const;
+
+    std::string_view GetSlice(std::string* scratch) const { 
+        switch (tag_)
+        {
+        case INT_TAG:
+            *scratch = std::to_string(u_.ival_);
+            return *scratch;
+        case STR_TAG:
+            return u_.str_;
+        case TTL_STR_TAG:
+            return u_.ttl_str_.str_;
+        default:
+            if (is_key_)
+                LOG(FATAL) << "Invalid tag: " << tag_;
+            return {};
+        } 
+
+    }
+
+
+protected:
+    void SetMeta(uint8_t tag) {
+        tag_ = tag;
+    }
+
+
 
     union U {
-        std::string str_;
+        std::string str_; // string不是POD类型，不能强制对齐， 如果使用ssd字符串会好些
 
-        TtlString str_ttl_;
+        TtlString ttl_str_;
+
+        int64_t ival_;
 
         U():str_(){}
         ~U(){} // 需要显式析构函数
     }u_;
 
     const bool is_key_ : 1; 
-    uint8_t taglen_ : 5;       
+    uint8_t tag_ : 5;       
 };
 
 struct CompactKey : public CompactObj {
@@ -111,7 +202,7 @@ struct CompactKey : public CompactObj {
     }
 
     bool HasExpire() const{
-    return taglen_ == SDS_TTL_TAG;
+    return tag_ == TTL_STR_TAG;
   }
 
     void SetExpireTime(uint64_t abs_ms);
